2092b58ab419bb74b453c32b0fcf42b5a6107830
Amanda Dean <86693254+Ajdean37@users.noreply.github.com>
2023-06-03 20:35:07 -0500

    Add files via upload

---
+++ pet-store/src/main/java/pet/store/PetStoreApplication.java
@@ -0,0 +1,14 @@
+package pet.store;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class PetStoreApplication extends SpringBootServletInitializer {
+ public static void main(String[] args) {
+  SpringApplication.run(PetStoreApplication.class, args);
+ }
+}
+
