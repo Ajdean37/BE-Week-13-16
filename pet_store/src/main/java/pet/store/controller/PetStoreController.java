bdec0e38a03ab6d25d0f932136f0dda8ee2ae218
Amanda Dean <86693254+Ajdean37@users.noreply.github.com>
2023-06-19 18:12:37 -0500

    Add files via upload

---
+++ pet_store/src/main/java/pet/store/controller/PetStoreController.java
@@ -0,0 +1,52 @@
+package pet.store.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import pet.store.controller.model.PetStoreData;
+import pet.store.entity.PetStore;
+import pet.store.service.PetStoreService;
+
+
+@RestController
+@RequestMapping("/pet_store")
+@Slf4j
+public class PetStoreController {
+
+ @Autowired
+ private PetStoreService petStoreService;
+
+ @PostMapping("/store")
+ @ResponseStatus(code = HttpStatus.CREATED)
+ public PetStoreData createPetStoreData(@RequestBody PetStoreData petStoreData) {
+  log.info("Creating Pet Store {}", petStoreData);
+  return petStoreService.savePetStore(petStoreData);
+ }
+
+ @PutMapping("/store/{petStoreId}")
+ @ResponseStatus(code = HttpStatus.CREATED)
+ public PetStoreData modifyPetStoreData(@PathVariable Long petStoreId, @RequestBody PetStoreData petStoreData) {
+  petStoreData.setPetStoreId(petStoreId);
+  log.info("Modifying Pet Store {}", petStoreData);
+  return petStoreService.savePetStore(petStoreData);
+ }
+
+ @PostMapping("/store/{petStoreId}/employee")
+ @ResponseStatus(code = HttpStatus.CREATED)
+ public PetStoreData.PetStoreEmployee addPetStoreEmployee(@PathVariable Long petStoreId,
+                                                          @RequestBody PetStoreData.PetStoreEmployee petStoreEmployee) {
+  log.info("Adding pet store employee {} {}", petStoreId, petStoreEmployee);
+  return petStoreService.saveEmployee(petStoreId, petStoreEmployee);
+
+ }
+
+ @PostMapping("/store/{petStoreId}/customer")
+ @ResponseStatus(code = HttpStatus.CREATED)
+ public PetStoreData.PetStoreCustomer addPetStoreCustomer(@PathVariable Long petStoreId,
+                                                          @RequestBody PetStoreData.PetStoreCustomer petStoreCustomer) {
+  log.info("Adding pet store employee {} {}", petStoreId, petStoreCustomer);
+  return petStoreService.saveEmployee(petStoreId, petStoreCustomer);
+ }
+
+}
