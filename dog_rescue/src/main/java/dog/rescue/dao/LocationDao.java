eec23411f7fea032b59873552dd4bd7de185342e
Amanda Dean <86693254+Ajdean37@users.noreply.github.com>
2023-06-20 10:01:25 -0500

    Delete dog_rescue directory

--- dog_rescue/src/main/java/dog/rescue/dao/LocationDao.java
+++
@@ -1,7 +0,0 @@
-package dog.rescue.dao;
-
-import dog.rescue.entity.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LocationDao extends JpaRepository<Location, Long> {
-}
